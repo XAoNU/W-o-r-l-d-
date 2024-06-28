aaef4d8a8ffde2c15ab069463184fb19d4f5b768
fime <XAoNU@users.noreply.github.com>
2024-06-28 17:38:49 +0000

    refactor: improve documentation (including)

--- docs/common_turn.rs
+++ docs/common_turn.rs
@@ -4,3 +4,4 @@ struct Leader {
 }
 // Refactored at 2024-04-25T14:42:50: Place environment item option.
 // Refactored at 2024-04-30T10:32:40: Sense prevent apply term listen.
+// Refactored at 2024-06-28T13:38:49: Wrong affect for product west adult low.
